4ca008423d51e90db882f3edf5bf76d40d61848b
kent <kent911t@gmail.com>
2024-10-28 23:29:59 +0000

    update main

--- Client/Client/Content/Aki/JavaScript/Game/Main.js
+++ Client/Client/Content/Aki/JavaScript/Game/Main.js
@@ -5,29 +5,28 @@ const puerts_1 = require("puerts"),
 UE = require("ue");
 
 async function mount() {
-    let modPath = UE.BlueprintPathsLibrary.RootDir() + "Mod";
-    let files = UE.KuroStaticLibrary.GetFilesRecursive(modPath, "*.pak");
+    const modPath = UE.BlueprintPathsLibrary.RootDir() + "Mod";
+    const files = UE.KuroStaticLibrary.GetFilesRecursive(modPath, "*.pak");
     for (let e = 0; e < files.Num(); e++) {
-        let f = files.Get(e);
-        UE.KuroPakMountStatic.MountPak(f, 1e5);
+        UE.KuroPakMountStatic.MountPak(files.Get(e), 1e5);
     }
 }
 
 async function main() {
     await mount();
-    setTimeout(() => {
+    // let the game launch if failed to load
+    try {
         // wait for mount, then this require will work
-        const Load = require("./ModMenu");
-        // let the game launch anyway
+        let Load = require("./ModMenu");
+        
         if (Load) {
             new Load.MainMenu({
                 loadFromLauncher: true,
             });
         }
-        
-        const GameProcedure_1 = require("./GameProcedure"); // this has to be here for the pak to load first
-        GameProcedure_1.GameProcedure.Start(puerts_1.argv.getByName("GameInstance"));
-    }, 250);
+    } catch {}
+    const GameProcedure_1 = require("./GameProcedure"); // this has to be here for the pak to load first
+    GameProcedure_1.GameProcedure.Start(puerts_1.argv.getByName("GameInstance"));
 }
 
 main();
